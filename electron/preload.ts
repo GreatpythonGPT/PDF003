d424b445126161f87eb721b47e25e270587b3966
GreatpythonGPT <maxkid@outlook.com>
2025-06-08 18:25:30 +0800

    先暂时清空

--- electron/preload.ts
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('api', {
-  selectImages: () => ipcRenderer.invoke('select-images'),
-  savePDF: (data: any) => ipcRenderer.invoke('save-pdf', data)
-});
